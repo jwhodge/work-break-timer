6fe60a5f2121af461f46937820c4056d699b9fae
jwhodge <jonathan@jonathanhodge.co.nz>
2021-09-29 16:24:34 +0800

    updated to near full function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import React from "react";
 import "./App.css";
 
-const sessionPeriod = 25;
-const breakPeriod = 5;
 const second = 1000;
 const minute = 60000;
+
+const buttonMap = [
+  {
+    class: "btn",
+    id: "session-increment",
+    icon: "",
+    value: "S+",
+    display: "Session Increment",
+  },
+  {
+    class: "btn",
+    id: "session-decrement",
+    icon: "",
+    value: "S-",
+    display: "Session Decrement",
+  },
+  {
+    class: "btn",
+    id: "break-increment",
+    icon: "",
+    value: "B+",
+    display: "Break Increment",
+  },
+  {
+    class: "btn",
+    id: "break-decrement",
+    icon: "",
+    value: "B-",
+    display: "Break Decrement",
+  },
+  {
+    class: "btn",
+    id: "start_stop",
+    icon: "",
+    value: "StopGo",
+    display: "Start/Stop",
+  },
+  {
+    class: "btn",
+    id: "reset",
+    icon: "",
+    value: "Reset",
+    display: "Reset",
+  },
+];
 
 function convertInputToMsecs(inputTime) {
   let msecs = Math.floor(inputTime * minute);
@@ -25,40 +68,83 @@ function convertDateToTime(delta, startTime, currentTime) {
   return toGo;
 }
 
+function convertTimeToDate(ms) {
+  let dateObj = new Date(ms);
+  return dateObj;
+}
+
+function countSeconds(startTime, currentTime) {
+  let start = Date.parse(startTime);
+  let current = Date.parse(currentTime);
+  let secsPassed = current - start;
+  return secsPassed;
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      sessionPeriod: 25,
+      breakPeriod: 5,
       date: new Date(),
+      clockDate: new Date(),
       startDate: new Date(),
-      counter: 0,
-      displayArr: [],
+      offsetDate: new Date(),
+      /* counters */
+      counter: 1500000,
+      offsetCounter: 0,
+      /* clock toggles */
+      clockRunning: false,
+      sessionNotBreak: true,
+      /* display */
+      displayArr: [25, 0],
     };
+    this.resetTimer = this.resetTimer.bind(this);
+    this.tick = this.tick.bind(this);
+    this.displayHandling = this.displayHandling.bind(this);
+    this.timerHandling = this.timerHandling.bind(this);
+    this.clickRouter = this.clickRouter.bind(this);
+    this.trackOffset = this.trackOffset.bind(this);
+    this.offsetTheDate = this.offsetTheDate.bind(this);
   }
 
   componentDidMount() {
     this.timerID = setInterval(() => {
       this.tick();
+      this.trackOffset();
       this.timerHandling();
       this.displayHandling();
+      this.handleSessionBreaks();
     }, 1000);
   }
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
+  resetTimer() {
+    this.setState(() => {
+      return {
+        sessionPeriod: 25,
+        breakPeriod: 5,
+        date: new Date(),
+        clockDate: new Date(),
+        startDate: new Date(),
+        /* counters */
+        counter: 1500000,
+        offsetCounter: 0,
+        /* clock toggles */
+        clockRunning: false,
+        sessionNotBreak: true,
+        /* display */
+        displayArr: [25, 0],
+      };
+    });
+  }
 
   tick() {
     this.setState({ date: new Date() });
-  }
-
-  timerHandling() {
-    this.setState({
-      counter: convertDateToTime(
-        convertInputToMsecs(sessionPeriod),
-        this.state.startDate,
-        this.state.date
-      ),
-    });
+    if (this.state.clockRunning) {
+      this.setState({ clockDate: new Date() });
+    }
   }
 
   displayHandling() {
@@ -67,21 +153,129 @@ class App extends React.Component {
     });
   }
 
+  trackOffset() {
+    this.setState({
+      offsetCounter: countSeconds(this.state.startDate, this.state.date),
+    });
+  }
+
+  offsetTheDate() {
+    let x = this.state.offsetCounter + Date.parse(this.state.startDate);
+    let date = convertTimeToDate(x);
+    console.log(date);
+    this.setState({
+      offsetDate: date,
+    });
+  }
+
+  timerHandling() {
+    if (this.state.clockRunning) {
+      if (this.state.sessionNotBreak) {
+        this.setState({
+          counter: convertDateToTime(
+            convertInputToMsecs(this.state.sessionPeriod),
+            this.state.offsetDate,
+            this.state.clockDate
+          ),
+        });
+      } else {
+        this.setState({
+          counter: convertDateToTime(
+            convertInputToMsecs(this.state.breakPeriod),
+            this.state.offsetDate,
+            this.state.clockDate
+          ),
+        });
+      }
+    }
+  }
+
+  clickRouter(switchCase) {
+    console.log("router input", switchCase);
+    let stateAddress = "";
+    let change = 0;
+    /* this switch dictates which state to change by what value */
+    switch (switchCase) {
+      case "S+":
+        change = 1;
+        stateAddress = "session";
+        break;
+      case "S-":
+        change = -1;
+        stateAddress = "session";
+        break;
+      case "B+":
+        change = 1;
+        stateAddress = "break";
+        break;
+      case "B-":
+        change = -1;
+        stateAddress = "break";
+        break;
+      case "StopGo":
+        change = 0;
+        stateAddress = "stopgo";
+        break;
+      case "Reset":
+        stateAddress = "reset";
+        break;
+      default:
+        console.log("incorrect input click router");
+    }
+    console.log("state", stateAddress, "change", change);
+    /* this switch selects the correct state and updates it */
+    switch (stateAddress) {
+      case "session":
+        if (!this.state.clockRunning) {
+          this.setState({
+            sessionPeriod: this.state.sessionPeriod + change,
+          });
+        }
+        break;
+      case "break":
+        if (!this.state.clockRunning) {
+          this.setState({
+            breakPeriod: this.state.breakPeriod + change,
+          });
+        }
+        break;
+      case "stopgo":
+        this.setState((prevState) => ({
+          clockRunning: !prevState.clockRunning,
+        }));
+        this.offsetTheDate();
+        break;
+      case "reset":
+        this.resetTimer();
+        break;
+      default:
+        console.log("bad stateAddress");
+    }
+  }
+
+  handleSessionBreaks() {
+    if (this.state.counter === 0) {
+      this.setState((prevState) => ({
+        sessionNotBreak: !prevState.sessionNotBreak,
+      }));
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1 id="app-title">Work/Break Timer</h1>
         <div className="Session">
           <h2 id="session-label">Session Length</h2>
-          <button id="session-increment">Session Increment</button>
-          <button id="session-decrement">Session Decrement</button>
-          <p id="session-length">25</p>
+          <Button btnInfo={buttonMap[0]} passHandler={this.clickRouter} />
+          <Button btnInfo={buttonMap[1]} passHandler={this.clickRouter} />
+          <p id="session-length">{this.state.sessionPeriod}</p>
         </div>
         <div className="Break">
           <h2 id="break-label">Break Length</h2>
-          <button id="break-increment">Break Increment</button>
-          <button id="break-decrement">Break Decrement</button>
-          <p id="break-length">5</p>
+          <Button btnInfo={buttonMap[2]} passHandler={this.clickRouter} />
+          <Button btnInfo={buttonMap[3]} passHandler={this.clickRouter} />
+          <p id="break-length">{this.state.breakPeriod}</p>
         </div>
         <div className="Timer">
           <h2 id="timer-label">Timer</h2>
@@ -90,8 +284,8 @@ class App extends React.Component {
           </div>
         </div>
         <div className="Control">
-          <button id="start_stop">Start/Stop</button>
-          <button id="reset">Reset</button>
+          <Button btnInfo={buttonMap[4]} passHandler={this.clickRouter} />
+          <Button btnInfo={buttonMap[5]} passHandler={this.clickRouter} />
         </div>
         <p>
           For the principles behind this work pattern see{" "}
@@ -105,6 +299,34 @@ class App extends React.Component {
         </p>
         <p>Built by Jonathan Hodge</p>
       </div>
+    );
+  }
+}
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.stopPropagation();
+    this.props.passHandler(e.target.value);
+  }
+
+  render() {
+    const btnArr = this.props.btnInfo;
+    return (
+      <button
+        className={btnArr.class}
+        id={btnArr.id}
+        value={btnArr.value}
+        onClick={this.handleClick}
+      >
+        {btnArr.display}
+      </button>
     );
   }
 }
---
+++ src/button-map.js
@@ -0,0 +1,44 @@
+const buttonMap = [
+  {
+    class: "btn",
+    id: "session-increment",
+    icon: "",
+    value: "S+",
+    display: "Session Increment",
+  },
+  {
+    class: "btn",
+    id: "session-decrement",
+    icon: "",
+    value: "S-",
+    display: "Session Decrement",
+  },
+  {
+    class: "btn",
+    id: "break-increment",
+    icon: "",
+    value: "B+",
+    display: "Break Increment",
+  },
+  {
+    class: "btn",
+    id: "break-decrement",
+    icon: "",
+    value: "B-",
+    display: "Break Decrement",
+  },
+  {
+    class: "btn",
+    id: "start_stop",
+    icon: "",
+    value: "StopGo",
+    display: "Start/Stop",
+  },
+  {
+    class: "btn",
+    id: "reset",
+    icon: "",
+    value: "Reset",
+    display: "Reset",
+  },
+];
